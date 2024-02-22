1c8155080bd17006c59bc04d57a8da15641b0e6b
gombar.egi <gombar.egi@gmail.com>
2024-02-22 13:06:08 +0100

    Create models and write equals definition

--- Models/Port.py
+++ Models/Port.py
@@ -1,9 +1,12 @@
 class Port:
-    def __init__(self, id, group_name, name, protocol, port, size, is_risky):
+    def __init__(self, id, name, protocol, port, size, is_risky):
         self.id = id
-        self.group_name = group_name
+        #self.group_name = group_name
         self.name = name
         self.protocol = protocol
         self.port = port
         self.size = size
         self.is_risky = is_risky
+    
+    def __eq__(self, __value: object) -> bool:
+        return self.protocol == __value.protocol and self.port == __value.port
