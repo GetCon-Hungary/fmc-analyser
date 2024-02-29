45a8b1c2553564693a5233e71eb9f3124ace6cf0
gombar.egi <gombar.egi@gmail.com>
2024-02-29 15:44:06 +0100

    rules tasks

--- Models/AccessPolicy.py
+++ Models/AccessPolicy.py
@@ -3,3 +3,29 @@ class AccessPolicy():
         self.name = name
         self.id = id
         self.rules = rules
+
+    def enabled_rules_count(self):
+        count = 0
+        for rule in self.rules:
+            if rule.enabled:
+                count += 1
+        return count
+    
+    def allowed_rules_count(self):
+        count = 0
+        for rule in self.rules:
+            if str.lower(rule.action) == "allow":
+                count += 1
+        return count
+    
+    def allowed_rules_ratio(self):
+        try:
+            return (self.allowed_rules_count() / len(self.rules)) * 100
+        except:
+            return "NaN"
+        
+    def enabled_rules_ratio(self):
+        try:
+            return self.enabled_rules_count() / len(self.rules) * 100
+        except:
+            return "NaN"
