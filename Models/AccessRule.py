83d7397bbd7a7700742ef3acd8c58bb5493c7010
gombar.egi <gombar.egi@gmail.com>
2024-03-21 09:40:09 +0100

    rename variable and fix defs

--- Models/AccessRule.py
+++ Models/AccessRule.py
@@ -40,32 +40,32 @@ class AccessRule:
         return self.network_used(network, self.source_networks) or self.network_used(network, self.destination_networks)
 
     def get_source_networks_size(self):
-        summ = 0
+        sum = 0
         for network in self.source_networks:
-            summ += network.get_size()
+            sum += network.get_size()
 
-        return summ
+        return sum
     
     def get_destination_network_size(self):
-        summ = 0
+        sum = 0
         for network in self.destination_networks:
-            summ += network.get_size()
+            sum += network.get_size()
 
-        return summ
+        return sum
     
     def get_source_port_size(self):
-        summ = 0
+        sum = 0
         for port in self.source_ports:
-            summ += port.get_size()
+            sum += port.get_size()
 
-        return summ
+        return sum
     
     def get_destination_port_size(self):
-        summ = 0
+        sum = 0
         for port in self.destination_ports:
-            summ += port.get_size()
+            sum += port.get_size()
 
-        return summ
+        return sum
     
     def risk_category_by_destination_port_dynamic(self, avg_port_number: int, relative_destination_port: dict[str, int]):
         if len(self.destination_ports) > 0:
@@ -79,20 +79,26 @@ class AccessRule:
             return Risk.High.name
     
     def risk_category_by_source_network_dynamic(self, avg_ip_number: int, relative_source_network: dict[str, int]):
-        if self.get_source_networks_size() >= relative_source_network['HIGH'] * avg_ip_number:
-            return Risk.High.name
-        elif self.get_source_networks_size() >= relative_source_network['MEDIUM'] * avg_ip_number:
-            return Risk.Medium.name
+        if len(self.source_networks) > 0:
+            if self.get_source_networks_size() >= relative_source_network['HIGH'] * avg_ip_number:
+                return Risk.High.name
+            elif self.get_source_networks_size() >= relative_source_network['MEDIUM'] * avg_ip_number:
+                return Risk.Medium.name
+            else:
+                return Risk.Low.name
         else:
-            return Risk.Low.name
+            return Risk.High.name
     
     def risk_category_by_destination_network_dynamic(self, avg_ip_number: int, relative_destination_network: dict[str, int]):
-        if self.get_destination_network_size() >= relative_destination_network['HIGH'] * avg_ip_number:
-            return Risk.High.name
-        elif self.get_destination_network_size() >= relative_destination_network['MEDIUM'] * avg_ip_number:
-            return Risk.Medium.name
+        if len(self.destination_networks) > 0:
+            if self.get_destination_network_size() >= relative_destination_network['HIGH'] * avg_ip_number:
+                return Risk.High.name
+            elif self.get_destination_network_size() >= relative_destination_network['MEDIUM'] * avg_ip_number:
+                return Risk.Medium.name
+            else:
+                return Risk.Low.name
         else:
-            return Risk.Low.name
+            return Risk.High.name
     
     def risk_category_by_destination_port_static(self, port_number: dict[str, int]):
         if len(self.destination_ports) > 0:
@@ -140,7 +146,7 @@ class AccessRule:
     
     def _calculate_subnet_mask(self, ip_number: int):
         mask = 32 - math.ceil(math.log2(ip_number))
-        if mask >= 0:
+        if mask > 0:
             return mask
         else:
             return 0
