9bb3f31d5406e2499485dfcd9c8910bb7b5ac34d
kerekes.barnabas <kerekes.barnabas@getcon.hu>
2024-03-21 22:04:33 +0100

    Style fixes, type hints, some docstrings

--- fmc_loader.py
+++ fmc_loader.py
@@ -1,8 +1,9 @@
+"""Gets all the necessary data from FMC."""
 import fmcapi
 
 
-class FMCLoader():
-    def __init__(self, fmc_host: str, username: str, password: str, acp_name: str):
+class FMCLoader:
+    def __init__(self, fmc_host: str, username: str, password: str, acp_name: str) -> None:
         with fmcapi.FMC(fmc_host,username=username, password=password, autodeploy=False) as fmc:
             self.protocol_port_objs = fmcapi.ProtocolPortObjects(fmc).get()
             self.port_obj_groups = fmcapi.PortObjectGroups(fmc).get()
@@ -13,7 +14,7 @@ class FMCLoader():
             self.access_policies = self.get_access_policies(fmc, acp_name)
             self.access_rules = self.get_access_rules(fmc)
 
-    def get_access_policies(self, fmc: fmcapi.FMC, acp_name: str):
+    def get_access_policies(self, fmc: fmcapi.FMC, acp_name: str) -> dict:
         access_policies = {}
         if acp_name == 'all':
             access_policies = fmcapi.AccessPolicies(fmc).get()
@@ -24,8 +25,9 @@ class FMCLoader():
             return access_policies
         raise NameError('Wrong access policy name')
 
-    def get_access_rules(self, fmc: fmcapi.FMC):
+    def get_access_rules(self, fmc: fmcapi.FMC) -> dict:
         access_rules = {}
         for access_policy in self.access_policies['items']:
-            access_rules[access_policy['name']] = fmcapi.AccessRules(fmc, acp_id=access_policy['id']).get()
+            access_rules[access_policy['name']] = fmcapi.AccessRules(
+                fmc, acp_id=access_policy['id']).get()
         return access_rules
